350a127a90074726c186b8bc4515962688eb6b74
larry brader <nullfire42@hotmail.com>
2018-01-18 10:19:41 -0800

    first save

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ZugApp
+{
+    class Program
+    {
+
+        static void Main (string[] args) 
+        {
+            Console.Writeline("Hello world!");
+        }    
+    }
+}
